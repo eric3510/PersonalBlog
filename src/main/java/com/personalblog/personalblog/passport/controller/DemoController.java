b89919f53a2fd04483ceff131a8ea8e0f917e878
Eric <eric351035@gmail.com>
2018-03-07 19:35:25 +0800

    在网页输出一个hello world的demo

---
+++ src/main/java/com/personalblog/personalblog/passport/controller/DemoController.java
@@ -0,0 +1,17 @@
+package com.personalblog.personalblog.passport.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/***
+ * @author 王强 Email : wangqiang@hushijie.com.cn
+ * @version 创建时间：2018/3/7
+ * DemoController
+ */
+@RestController
+public class DemoController{
+    @RequestMapping("/")
+    public String home(){
+        return "hello world";
+    }
+}
